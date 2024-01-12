805c29d268b64d2dad9b905ee065fa7064494726
phongtruong2106 <thpgamer42@gmail.com>
2024-01-12 18:40:03 +0700

    Update Fix speed Shooting

--- Assets/Script/Ship/ShipShooting.cs
+++ Assets/Script/Ship/ShipShooting.cs
@@ -4,15 +4,25 @@ public class ShipShooting : MonoBehaviour
 {
     [SerializeField] protected bool isShooting = false;
     [SerializeField] protected Transform bulletPrefab;
+    [SerializeField] protected float shootDelay = 1f;
+    [SerializeField] protected float shootTimer = 1f;
     
     private void Update() {
-        this.IsShooting();
+        this.IsShooting();   
+        
+    }
+    private void FixedUpdate()
+    {
         this.Shooting();
     }
-
     protected virtual void Shooting()
     {
         if(!this.isShooting) return;
+
+        this.shootTimer += Time.fixedDeltaTime;
+        if(this.shootTimer < this.shootDelay) return;
+        this.shootTimer = 0;
+
         Vector3 spawnPos = transform.position;
         Quaternion rotation = transform.parent.rotation;
         Instantiate(this.bulletPrefab, spawnPos, rotation);

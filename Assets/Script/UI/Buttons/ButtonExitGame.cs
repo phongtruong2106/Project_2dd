1a5afb19a1985681fc0c18b831894972a137d3e1
phongtruong2106 <91039342+phongtruong2106@users.noreply.github.com>
2024-01-27 22:36:22 +0700

    Update UI Btn Exit

--- Assets/Script/UI/Buttons/ButtonExitGame.cs
+++ Assets/Script/UI/Buttons/ButtonExitGame.cs
@@ -2,7 +2,11 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class ButtonExitGame : NewMonobehavior
+public class ButtonExitGame : BaseButton
 {
-    
+    protected override void OnClick()
+    {
+        Debug.Log("OnCLick Button Exits");
+        Application.Quit();
+    }
 }

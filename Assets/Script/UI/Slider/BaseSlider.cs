6578491f46bd3d76717587d997135c0e502ad66a
phongtruong2106 <91039342+phongtruong2106@users.noreply.github.com>
2024-01-28 00:10:26 +0700

    Update Base Slider

---
+++ Assets/Script/UI/Slider/BaseSlider.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public abstract class BaseSlider : NewMonobehavior
+{
+    [Header("Base Slider")]
+    [SerializeField] protected Slider slider;
+
+    protected override void Start()
+    {
+        base.Start();
+        this.AddOnClickEvent();
+    }
+    protected override void LoadComponents()
+    {
+        base.LoadComponents();
+        this.LoadSlider();
+    }
+
+    protected virtual void LoadSlider()
+    {
+        if(this.slider != null) return;
+        this.slider = GetComponent<Slider>();
+        Debug.LogWarning(transform.name + ": LoadSlider", gameObject);
+    }
+
+    protected virtual void AddOnClickEvent()
+    {
+        this.slider.onValueChanged.AddListener(this.OnChanged);
+    }
+
+    protected abstract void OnChanged(float newValue);
+}
---
+++ Assets/Script/UI/Slider/SliderHP.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SliderHP: BaseSlider
+{
+    [Header("HP")]
+    [SerializeField] protected float maxHP = 100;
+    [SerializeField] protected float currentHP = 70; 
+
+    protected virtual void FixedUpdate()
+    {
+        this.ShowHP();
+    }
+
+    protected virtual void ShowHP()
+    {
+        float hpPercent = this.currentHP /  this.maxHP;
+        this.slider.value = hpPercent;
+    }
+    protected override void OnChanged(float newValue)
+    {
+        Debug.Log("NewValue: " + newValue);
+    }
+
+    public virtual void SetMaxHP(float maxHP)
+    {
+        this.maxHP = maxHP;
+    }
+    
+    public virtual void SetCurrentHP(float currentHP)
+    {
+        this.currentHP = currentHP;
+    }
+}

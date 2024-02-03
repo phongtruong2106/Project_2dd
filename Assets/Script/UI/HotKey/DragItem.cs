ae44fd5e35699a7e56f3ac3783207030f4f31666
phongtruong2106 <91039342+phongtruong2106@users.noreply.github.com>
2024-02-03 19:17:45 +0700

    Update Drag ITEM

---
+++ Assets/Script/UI/HotKey/DragItem.cs
@@ -0,0 +1,23 @@
+using UnityEngine;
+using UnityEngine.EventSystems;
+public class DragItem : NewMonobehavior, IBeginDragHandler, IDragHandler, IEndDragHandler
+{
+    public void OnBeginDrag(PointerEventData eventData)
+    {
+        Debug.Log("OnBeginDrag");
+    }
+
+    public void OnDrag(PointerEventData eventData)
+    {
+        Debug.Log("OnDrag");  
+        Vector3 mousePos = InputManager.Instance.MouseWorldPos;
+        mousePos.z = 0;
+        transform.position = mousePos;  
+    }
+
+    public void OnEndDrag(PointerEventData eventData)
+    {
+        Debug.Log("OnEndDrag");
+
+    }
+}
